e021b6d082c94ad9d0899d17c43f4347ae82a9b9
qweliant <qweliantanner@gmail.com>
2020-04-02 19:07:41 -0400

    set procfile

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn chatume.wsgi
+run_migrate: python manage.py makemigrations
+run_train: python manage.py train
--- chatume/settings.py
+++ chatume/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,5 +138,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    "/home/youth/MEGA/codin/Coding/TraceCamp/project2/chatume/chat/static/",
+    os.path.join(BASE_DIR, "static"),
+    '/chat/static/',
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
