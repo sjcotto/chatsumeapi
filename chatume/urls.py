e184a100c4d13cb186522d843482f3977002fb6b
qweliant <qweliantanner@gmail.com>
2020-04-02 21:22:31 -0400

    TODO: fix csrf

--- chatume/urls.py
+++ chatume/urls.py
@@ -6,6 +6,6 @@ from chat.views import ChatterBotAppView, ChatterBotApiView
 urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view(), name='main'),
     url(r'^admin/', admin.site.urls, name='admin'),
-    url(r'^api/chatterbot/', ChatterBotApiView.as_view(), name='chatterbot'),
+    url(r'^api/chatterbot/', ChatterBotApiView.as_view(), name='chatume'),
 ]
 
