f6461ccc188e1ae02ae000b417d4303ee1e151f6
qweliant <qweliantanner@gmail.com>
2020-05-19 19:32:55 -0400

    set up flask, switching to docker deploy

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from chat.models import Post
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+    def testPost(self):
+        post = Post(title="My Title", description="Blurb", wiki="Post Body")
+        self.assertEqual(post.title, "My Title")
+        self.assertEqual(post.description, "Blurb")
+        self.assertEqual(post.wiki, "Post Body")
