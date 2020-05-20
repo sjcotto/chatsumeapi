66c4685cc9f48704b2b169b2290676da0e2697cc
qweliant <qweliantanner@gmail.com>
2020-05-20 01:29:42 -0400

    docker and more errors

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+
+FROM python:3.7.7-alpine
+COPY . /app
+WORKDIR /app
+RUN pipenv install
+EXPOSE 5001 
+ENTRYPOINT [ "python" ] 
+CMD [ "app.py" ] 
--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def get_bot_response():
     return jsonify(results = str(response))
 
 if __name__ == "__main__":    
-    app.run()
+    app.run(host ='0.0.0.0', port = 5001, debug = True)
