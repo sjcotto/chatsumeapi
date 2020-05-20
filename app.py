63ce61c1036090e30e88776909bf65b82e415e3f
qweliant <qweliantanner@gmail.com>
2020-05-19 23:00:21 -0400

    update package

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ from train import bot
 
 app = Flask(__name__)
 
-
-
-
 @app.route("/", methods = ['POST'])
 @cross_origin()
 def get_bot_response():    
     data = request.get_json(force = True)  
     response = bot.get_response(data["mssg"])
-    # print(response)
     return jsonify(results = str(response))
 
 if __name__ == "__main__":    
