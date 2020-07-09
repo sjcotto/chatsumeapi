f96869bc47fc7df425c5158871232c5421a9c3e6
qweliant <qweliantanner@gmail.com>
2020-07-09 01:31:57 -0400

    api connected

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from fastapi import FastAPI
-
-
-# nlp = NLP()
-app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 from train import bot
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    mssg: str
+
+app = FastAPI()
+
+origins = [
+
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.post("/chat/")
-async def get_bot_response(msg: str):    
-    # response = bot.get_response(data["mssg"])
-    return {"chat": msg}
+async def bot_mssg(item: Item):
+    print(item.mssg)
+    # response = bot.get_response(msg)
+    # print("Res", response)
+    return item.mssg
 
 @app.get("/ner/")
 async def  named_entity_recognition():
     return {"ner": "NER"}
-
-if __name__ == "__main__":    
-    app.run(host ='0.0.0.0', port = 5001, debug = True)
