ecd1a66fc70a3f265916bd9883b0970fb3322ff4
Qwelian Tanner <qweliantanner@gmail.com>
2020-07-27 10:50:58 -0400

    port set up

--- app/api.py
+++ app/api.py
@@ -15,7 +15,8 @@ origins = [
     "https://www.metavisuo.com/nlp",
     "https://www.metavisuo.com",
     "http://localhost",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 
 app.add_middleware(
